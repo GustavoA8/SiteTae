daaa9be7fc2c0d1ece79300bf45a7ce3bc2037d5
Monitoramento05 <monitoramento05@tae.local>
2024-05-24 17:58:30 -0300

    style: melhorado navbar e section training no mobile

--- js/script.js
+++ js/script.js
@@ -1,44 +1,27 @@
-verificar = false
-popup = null 
-function dropdown() {
-    if (verificar == false) {
-        console.log('funcionando')
-        const btn = document.getElementsByClassName('dropdown-content')[0]
-        btn.style.display = 'block'
-        const sectionShield = document.getElementsByClassName('section-shield')[0]
-        sectionShield.style.marginTop = '590px'
-        const barra = document.getElementsByClassName('btnimg')[0]
-        barra.style.marginRight = '500px'
-        verificar = true
-    }else{
-        console.log("fechando")
-        const btn = document.getElementsByClassName('dropdown-content')[0]
-        btn.style.display = 'none'
-        const sectionShield = document.getElementsByClassName('section-shield')[0]
-        sectionShield.style.marginTop = '0'
-        const barra = document.getElementsByClassName('btnimg')[0]
-        barra.style.marginRight = '0'
-        verificar = false
-    }
+// Altera visibilidade da navbar mobile ao clicar no menu hamburguer
+
+document.getElementById("dropdown").addEventListener("click", ()=>{
+
+    document.getElementsByClassName("navbar-mobile-opcoes-lista")[0].classList.toggle("invisivel");
+
+});
 
 
-}
+// Altera visiblidade das redes sociais no desktop
+document.getElementById("redes-sociais-dropDown").addEventListener("click", ()=>{
 
 
-document.getElementById("redes-sociais-dropDown").addEventListener("click", ()=>{mostrarRedes("sim")});
-
-
-function mostrarRedes(tipo){
     let midiasContainer = document.getElementById("midia-icones-container");
     let setaDropDown = document.getElementById("social-media-dropDown-icone");
 
     midiasContainer.classList.toggle("mostrar");
     setaDropDown.classList.toggle("seta-acionada");
+    
+});
 
 
 
-   
-}
+
 
 function popupNO(){
     console.log("funcionando")
