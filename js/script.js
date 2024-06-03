61c07662ecefb8f21a88037b527d681567766068
Monitoramento05 <monitoramento05@tae.local>
2024-06-03 17:57:37 -0300

    enviado ao wordpress

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-// Altera visibilidade da navbar mobile ao clicar no menu hamburguer
-document.getElementById("dropdown").addEventListener("click", ()=>{
+document.getElementById("dropdown").addEventListener("click", ligarNavMobile);
+
+
+function ligarNavMobile(){
 
     document.getElementsByClassName("navbar-mobile-opcoes-lista")[0].classList.toggle("invisivel");
     document.getElementsByClassName("hambuguer-img")[0].classList.toggle("ativado");
 
-});
+}
+
+// Altera visibilidade da navbar mobile ao clicar no menu hamburguer
+document.getElementById("dropdown").addEventListener("click", ligarNavMobile);
+
 
 
 // Altera visiblidade das redes sociais no desktop
 document.getElementById("redes-sociais-dropDown").addEventListener("click", ()=>{
-
 
     let midiasContainer = document.getElementById("midia-icones-container");
     let setaDropDown = document.getElementById("social-media-dropDown-icone");
@@ -20,7 +25,6 @@ document.getElementById("redes-sociais-dropDown").addEventListener("click", ()=>
 });
 
 document.getElementById("scroll-social-media-btn").addEventListener("click", ()=>{
-    console.log("buirg")
     document.getElementsByClassName("sociais")[0].scrollIntoView();
 
 })
@@ -30,7 +34,6 @@ function scrollHomePage(pagina){
     switch (pagina) {
         case "about":
             document.getElementsByClassName("section-about-us")[0].scrollIntoView();
-
             break;
         case "social":
             document.getElementsByClassName("sociais")[0].scrollIntoView();
@@ -51,7 +54,6 @@ function scrollHomePage(pagina){
 
 
 function popupNO(){
-    console.log("funcionando")
     body = document.getSelection("body")
     popupElement = document.getElementsByClassName("pop-up")[0]
     let msg = document.getElementById("errorAge")
@@ -64,12 +66,49 @@ function popupNO(){
 
 }
 function popupYES(){
-    console.log("funcionando")
-
     popupElement = document.getElementsByClassName("pop-up")[0]
 
     
     popupElement.style.display = "none"
 }
 
-// console.log(document.getElementById("redes-sociais-dropDown"))
+
+
+// Impedir erro de declarar a mesma variavel 2x
+if (typeof options === 'undefined') {
+    var options = {
+        root: null,
+        threshold: 0,
+        rootMargin: "-400px 0px 0px 0px"
+    };
+
+    
+    var landing = document.getElementById("titulo");
+    var shield = document.getElementById("shieldPag");
+
+
+    // animar nav-bar conforme scroll na tela
+    var observandoNav = new IntersectionObserver(function(entries, observer){
+        entries.forEach(entry =>{
+            const nav = document.querySelector("header");
+            if(entry.isIntersecting){
+                nav.classList.remove("scrollNav");
+            }else{
+                nav.classList.add("scrollNav");
+                // hamburguer.classList.add("hamburguerScrolled");
+        }
+        })
+    }, options);
+}
+
+
+
+try {
+    observandoNav.observe(landing);
+
+} catch (error) {
+    observandoNav.observe(shield);
+}
+
+
+window.observandoNav = observandoNav;
